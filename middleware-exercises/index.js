48ec80053b1d76cf29bac9650db71ed0b8864148
Craig D'Silva <craig.dsilva10@gmail.com>
2026-06-01 11:59:56 +0100

    X-Username header

--- middleware-exercises/index.js
+++ middleware-exercises/index.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.status(200).send("Hello World!"));
+app.get("/", (req, res) => res.status(200).send("Server is running"));
+
+app.post("/", (req, res) => {
+  req.username = req.headers["x-username"] ?? null;
+  next();
+});
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
