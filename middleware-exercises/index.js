e9f35c7a65845e02e4a7d306b2087ead462b8802
Craig D'Silva <craig.dsilva10@gmail.com>
2026-06-01 12:55:19 +0100

    Body middleware

--- middleware-exercises/index.js
+++ middleware-exercises/index.js
@@ -6,12 +6,32 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.status(200).send("Server is running"));
 
-app.post("/", (req, res) => {
-  req.username = req.headers["x-username"] ?? null;
-  const message = req.username
-    ? `You are authenticated as ${req.username}`
-    : "You are not authenticated";
-  res.status(200).send(message);
-});
+app.post(
+  "/",
+  (req, res, next) => {
+    req.username = req.headers["x-username"] ?? null;
+    if (
+      !Array.isArray(req.body) ||
+      !req.body.every((item) => typeof item === "string")
+    ) {
+      return res
+        .status(400)
+        .json({ error: "Request body must be a JSON array of strings" });
+    }
+    res.locals.body = req.body;
+    next();
+  },
+  (req, res) => {
+    const auth = req.username
+      ? `You are authenticated as ${req.username}`
+      : "You are not authenticated";
+    const data = res.locals.body;
+    res
+      .status(200)
+      .send(
+        `${auth}\n \nYou have requested information about ${data.length} ${data.length === 1 ? "subject" : "subjects"}: ${data} `,
+      );
+  },
+);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
