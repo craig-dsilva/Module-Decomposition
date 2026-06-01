7329330c4313a7c6fe746ae24b6e7cf17b5dd108
Craig D'Silva <craig.dsilva10@gmail.com>
2026-06-01 12:23:38 +0100

    X-Username header

--- middleware-exercises/index.js
+++ middleware-exercises/index.js
@@ -8,7 +8,10 @@ app.get("/", (req, res) => res.status(200).send("Server is running"));
 
 app.post("/", (req, res) => {
   req.username = req.headers["x-username"] ?? null;
-  next();
+  const message = req.username
+    ? `You are authenticated as ${req.username}`
+    : "You are not authenticated";
+  res.status(200).send(message);
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
