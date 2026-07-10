ceebe048c6f1dbd82ee92c6667131e03e9c2dd9b
Craig D'Silva <craigdsilva@proton.me>
2026-07-10 16:09:59 +0100

    Format

--- middleware-exercises/index.js
+++ middleware-exercises/index.js
@@ -6,17 +6,13 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.status(200).send("Server is running"));
 
-app.post(
-  "/",
-  (req, res, next) => {
+app.post("/",(req, res, next) => {
     req.username = req.headers["x-username"] ?? null;
     if (
       !Array.isArray(req.body) ||
       !req.body.every((item) => typeof item === "string")
     ) {
-      return res
-        .status(400)
-        .json({ error: "Request body must be a JSON array of strings" });
+      return res.status(400).json({ error: "Request body must be a JSON array of strings" });
     }
     res.locals.body = req.body;
     next();
@@ -26,11 +22,7 @@ app.post(
       ? `You are authenticated as ${req.username}`
       : "You are not authenticated";
     const data = res.locals.body;
-    res
-      .status(200)
-      .send(
-        `${auth}\n \nYou have requested information about ${data.length} ${data.length === 1 ? "subject" : "subjects"}: ${data} `,
-      );
+    res.status(200).send(`${auth}\n \nYou have requested information about ${data.length} ${data.length === 1 ? "subject" : "subjects"}: ${data} `);
   },
 );
 
